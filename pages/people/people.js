2024ebe9f08ab91a2d7bc9405ac0dfbf2d8e9ba0
antqi <hi.antqi@gmail.com>
2018-10-17 17:44:11 +0800

    + people & test svg

---
+++ pages/people/people.js
@@ -0,0 +1,290 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "userList": [
+      {
+        "realName": "郭洪波",
+        "orgName": "享学教育",
+        "phone": "15011111111",
+        "createTime": "2018-08-07 18:00:58",
+        "name": "享学教育",
+        "roleName": "erp测试",
+        "createUser": "manager",
+        "updateTime": "2018-08-07 18:00:58",
+        "id": 92,
+        "orgId": 1,
+        "username": "guohb",
+        "status": 1
+      },
+      {
+        "orgName": "享学教育",
+        "updateUser": "admin",
+        "updateTime": "2018-06-20 17:16:43",
+        "orgId": 1,
+        "realName": "wxw",
+        "phone": "15845326974",
+        "createTime": "2018-06-20 17:15:55",
+        "name": "享学教育",
+        "roleName": "测试角色",
+        "createUser": "admin",
+        "id": 91,
+        "username": "wxw",
+        "status": 1
+      },
+      {
+        "orgName": "享学教育",
+        "updateUser": "admin",
+        "updateTime": "2018-06-20 13:33:00",
+        "orgId": 1,
+        "realName": "seyy_admin",
+        "phone": "13412312333",
+        "createTime": "2018-06-20 11:39:59",
+        "name": "享学教育",
+        "roleName": "只有少儿英语",
+        "createUser": "admin",
+        "id": 90,
+        "username": "seyy_admin",
+        "status": 1
+      },
+      {
+        "orgName": "享学教育",
+        "updateUser": "admin",
+        "updateTime": "2018-06-20 13:33:07",
+        "orgId": 1,
+        "realName": "crm_admin",
+        "phone": "18728305408",
+        "createTime": "2018-06-20 11:39:39",
+        "name": "享学教育",
+        "roleName": "只有CRM",
+        "createUser": "admin",
+        "id": 89,
+        "username": "crm_admin",
+        "status": 1
+      },
+      {
+        "orgName": "享学教育",
+        "updateUser": "admin",
+        "updateTime": "2018-06-20 13:33:14",
+        "orgId": 1,
+        "realName": "gxh_admin",
+        "phone": "18232138123",
+        "createTime": "2018-06-20 11:39:04",
+        "name": "享学教育",
+        "roleName": "只有个性化",
+        "createUser": "admin",
+        "id": 88,
+        "username": "gxh_admin",
+        "status": 1
+      },
+      {
+        "orgName": "享学教育",
+        "updateUser": "admin",
+        "updateTime": "2018-06-20 13:33:22",
+        "orgId": 1,
+        "realName": "py_admin",
+        "phone": "18239021309",
+        "createTime": "2018-06-20 11:38:43",
+        "name": "享学教育",
+        "roleName": "只有培英",
+        "createUser": "admin",
+        "id": 87,
+        "username": "py_admin",
+        "status": 1
+      },
+      {
+        "orgName": "jy",
+        "updateUser": "hjy2",
+        "updateTime": "2018-05-29 14:50:02",
+        "orgId": 844,
+        "realName": "hjy3",
+        "phone": "18698786841",
+        "createTime": "2018-05-29 14:50:02",
+        "name": "jy",
+        "roleName": "",
+        "createUser": "hjy2",
+        "id": 83,
+        "username": "hjy3",
+        "status": 1
+      },
+      {
+        "orgName": "jy",
+        "updateUser": "hjy",
+        "updateTime": "2018-05-29 14:46:44",
+        "orgId": 844,
+        "realName": "hjy2",
+        "phone": "18568968968",
+        "createTime": "2018-05-29 14:46:44",
+        "name": "jy",
+        "roleName": "jy1",
+        "createUser": "hjy",
+        "id": 82,
+        "username": "hjy2",
+        "status": 1
+      },
+      {
+        "orgName": "cca-1",
+        "updateUser": "cca1",
+        "updateTime": "2018-05-24 20:51:52",
+        "orgId": 839,
+        "realName": "发发发",
+        "phone": "15678900064",
+        "createTime": "2018-05-24 20:51:52",
+        "name": "cca-1",
+        "roleName": "",
+        "createUser": "cca1",
+        "id": 81,
+        "username": "fff",
+        "status": 1
+      },
+      {
+        "orgName": "享学教育",
+        "updateUser": "caihuan1",
+        "updateTime": "2018-05-23 18:17:07",
+        "orgId": 1,
+        "realName": "hjy",
+        "phone": "18864757879",
+        "createTime": "2018-05-23 18:17:07",
+        "name": "享学教育",
+        "roleName": "测试",
+        "createUser": "caihuan1",
+        "id": 80,
+        "username": "hjy1",
+        "status": 1
+      },
+      {
+        "orgName": "jy",
+        "updateUser": "cca",
+        "updateTime": "2018-06-19 17:24:04",
+        "orgId": 844,
+        "realName": "hjy",
+        "phone": "18801613586",
+        "createTime": "2018-05-23 18:12:02",
+        "name": "jy",
+        "roleName": "jy管理员",
+        "createUser": "cca",
+        "id": 79,
+        "username": "hjy",
+        "status": 1
+      },
+      {
+        "orgName": "cca-1",
+        "updateUser": "caihuan1",
+        "updateTime": "2018-06-19 17:24:04",
+        "orgId": 839,
+        "realName": "cca",
+        "phone": "15677778899",
+        "createTime": "2018-05-23 14:02:56",
+        "name": "cca-1",
+        "roleName": "cca-1管理员",
+        "createUser": "caihuan1",
+        "id": 78,
+        "username": "cca1",
+        "status": 1
+      },
+      {
+        "orgName": "享学教育",
+        "updateUser": "caihuan1",
+        "updateTime": "2018-05-23 14:02:09",
+        "orgId": 1,
+        "realName": "cca",
+        "phone": "18966337755",
+        "createTime": "2018-05-23 13:58:02",
+        "name": "享学教育",
+        "roleName": "测试k",
+        "createUser": "caihuan1",
+        "id": 77,
+        "username": "cca",
+        "status": 1
+      },
+      {
+        "orgName": "个性化",
+        "updateUser": "admin",
+        "updateTime": "2018-06-20 13:59:11",
+        "orgId": 6,
+        "realName": "gxAdmin",
+        "phone": "15000000002",
+        "createTime": "2018-05-23 10:52:34",
+        "name": "个性化",
+        "roleName": "所有权限",
+        "createUser": "admin",
+        "id": 13,
+        "username": "gxh",
+        "status": 1
+      },
+      {
+        "orgName": "少儿英语",
+        "updateUser": "admin",
+        "updateTime": "2018-05-28 17:56:44",
+        "orgId": 5,
+        "realName": "seAdmin",
+        "phone": "15000000001",
+        "createTime": "2018-05-23 10:51:41",
+        "name": "少儿英语",
+        "roleName": "优惠券权限",
+        "createUser": "admin",
+        "id": 12,
+        "username": "seyy",
+        "status": 1
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
